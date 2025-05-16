4fa85f900e1dd6a6925dd74e1e9d337c95191dd2
jordanAlava <jordan.alava@espoch.edu.ec>
2025-05-15 17:07:17 -0700

    esta es una prueba JA

---
+++ capaDato/ClassConexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDato
+{
+    internal class ClassConexion
+    {
+        // esta es la nueva clase
+    }
+}
