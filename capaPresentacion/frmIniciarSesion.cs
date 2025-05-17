29e66b751752a9a05c0227a40a2f9c8be0822f31
Gonzalo Romero <gabriel.romero@esppoch.edu.ec>
2025-05-17 14:05:24 -0500

    frmIniciarSesion primer diseño - GR

--- capaPresentacion/frmIniciarSesion.cs
+++ capaPresentacion/frmIniciarSesion.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+
 
 namespace capaPresentacion
 {
     public partial class frmIniciarSesion : Form
     {
+        public string rutaRepositorio = "https://raw.githubusercontent.com/jordanAlava/proyectoServicioMecanica/";
         public frmIniciarSesion()
         {
+
             InitializeComponent();
+            inicializarFormulario();
+        }
+
+        public void inicializarFormulario()
+        {
+            pctrLogotipo.Image = Image.FromFile(rutaRepositorio + "main/Recursos/Imagenes/logotipo.jpg");
         }
     }
 }
