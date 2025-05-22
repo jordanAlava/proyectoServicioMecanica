f4452dcad9e4ca73ff49d37cf6349084403497cc
jordanAlava <jordan.alava@espoch.edu.ec>
2025-05-22 00:01:52 -0700

    ayuda y acerca de

--- capaPresentacion/cliente/frmClientePantallaPrincipal.Designer.cs
+++ capaPresentacion/cliente/frmClientePantallaPrincipal.Designer.cs
@@ -59,17 +59,19 @@
             this.pnlMenuLateral.Controls.Add(this.pnlLogotipo);
             this.pnlMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenuLateral.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlMenuLateral.Name = "pnlMenuLateral";
-            this.pnlMenuLateral.Size = new System.Drawing.Size(264, 681);
+            this.pnlMenuLateral.Size = new System.Drawing.Size(352, 838);
             this.pnlMenuLateral.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.btnCerrarSesion);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 588);
+            this.panel2.Location = new System.Drawing.Point(0, 724);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(264, 53);
+            this.panel2.Size = new System.Drawing.Size(352, 65);
             this.panel2.TabIndex = 8;
             // 
             // btnCerrarSesion
@@ -86,9 +88,10 @@
             this.btnCerrarSesion.IconSize = 36;
             this.btnCerrarSesion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnCerrarSesion.Location = new System.Drawing.Point(0, 0);
+            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Padding = new System.Windows.Forms.Padding(70, 0, 30, 0);
-            this.btnCerrarSesion.Size = new System.Drawing.Size(264, 53);
+            this.btnCerrarSesion.Padding = new System.Windows.Forms.Padding(93, 0, 40, 0);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(352, 65);
             this.btnCerrarSesion.TabIndex = 7;
             this.btnCerrarSesion.Text = "Cerrar Sesión";
             this.btnCerrarSesion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -99,9 +102,10 @@
             // panel1
             // 
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 641);
+            this.panel1.Location = new System.Drawing.Point(0, 789);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(264, 40);
+            this.panel1.Size = new System.Drawing.Size(352, 49);
             this.panel1.TabIndex = 5;
             // 
             // btnAcercaDe
@@ -112,14 +116,16 @@
             this.btnAcercaDe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAcercaDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAcercaDe.ForeColor = System.Drawing.Color.White;
-            this.btnAcercaDe.Location = new System.Drawing.Point(0, 294);
+            this.btnAcercaDe.Location = new System.Drawing.Point(0, 363);
+            this.btnAcercaDe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAcercaDe.Name = "btnAcercaDe";
-            this.btnAcercaDe.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnAcercaDe.Size = new System.Drawing.Size(264, 50);
+            this.btnAcercaDe.Padding = new System.Windows.Forms.Padding(27, 0, 0, 0);
+            this.btnAcercaDe.Size = new System.Drawing.Size(352, 62);
             this.btnAcercaDe.TabIndex = 4;
             this.btnAcercaDe.Text = "Acerca de";
             this.btnAcercaDe.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAcercaDe.UseVisualStyleBackColor = false;
+            this.btnAcercaDe.Click += new System.EventHandler(this.btnAcercaDe_Click);
             // 
             // btnAyuda
             // 
@@ -129,14 +135,16 @@
             this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAyuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAyuda.ForeColor = System.Drawing.Color.White;
-            this.btnAyuda.Location = new System.Drawing.Point(0, 244);
+            this.btnAyuda.Location = new System.Drawing.Point(0, 301);
+            this.btnAyuda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAyuda.Name = "btnAyuda";
-            this.btnAyuda.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnAyuda.Size = new System.Drawing.Size(264, 50);
+            this.btnAyuda.Padding = new System.Windows.Forms.Padding(27, 0, 0, 0);
+            this.btnAyuda.Size = new System.Drawing.Size(352, 62);
             this.btnAyuda.TabIndex = 3;
             this.btnAyuda.Text = "Ayuda";
             this.btnAyuda.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAyuda.UseVisualStyleBackColor = false;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
             // 
             // btnAgendarServicio
             // 
@@ -146,10 +154,11 @@
             this.btnAgendarServicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgendarServicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgendarServicio.ForeColor = System.Drawing.Color.White;
-            this.btnAgendarServicio.Location = new System.Drawing.Point(0, 194);
+            this.btnAgendarServicio.Location = new System.Drawing.Point(0, 239);
+            this.btnAgendarServicio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAgendarServicio.Name = "btnAgendarServicio";
-            this.btnAgendarServicio.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnAgendarServicio.Size = new System.Drawing.Size(264, 50);
+            this.btnAgendarServicio.Padding = new System.Windows.Forms.Padding(27, 0, 0, 0);
+            this.btnAgendarServicio.Size = new System.Drawing.Size(352, 62);
             this.btnAgendarServicio.TabIndex = 2;
             this.btnAgendarServicio.Text = "Agendar Servicio";
             this.btnAgendarServicio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -164,10 +173,11 @@
             this.btnMisVehiculos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMisVehiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMisVehiculos.ForeColor = System.Drawing.Color.White;
-            this.btnMisVehiculos.Location = new System.Drawing.Point(0, 144);
+            this.btnMisVehiculos.Location = new System.Drawing.Point(0, 177);
+            this.btnMisVehiculos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMisVehiculos.Name = "btnMisVehiculos";
-            this.btnMisVehiculos.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnMisVehiculos.Size = new System.Drawing.Size(264, 50);
+            this.btnMisVehiculos.Padding = new System.Windows.Forms.Padding(27, 0, 0, 0);
+            this.btnMisVehiculos.Size = new System.Drawing.Size(352, 62);
             this.btnMisVehiculos.TabIndex = 1;
             this.btnMisVehiculos.Text = "Mis Vehículos";
             this.btnMisVehiculos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -180,15 +190,17 @@
             this.pnlLogotipo.Controls.Add(this.pctrLogotipo);
             this.pnlLogotipo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlLogotipo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogotipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlLogotipo.Name = "pnlLogotipo";
-            this.pnlLogotipo.Size = new System.Drawing.Size(264, 144);
+            this.pnlLogotipo.Size = new System.Drawing.Size(352, 177);
             this.pnlLogotipo.TabIndex = 0;
             // 
             // pctrLogotipo
             // 
-            this.pctrLogotipo.Location = new System.Drawing.Point(-20, -33);
+            this.pctrLogotipo.Location = new System.Drawing.Point(-27, -41);
+            this.pctrLogotipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pctrLogotipo.Name = "pctrLogotipo";
-            this.pctrLogotipo.Size = new System.Drawing.Size(287, 171);
+            this.pctrLogotipo.Size = new System.Drawing.Size(383, 210);
             this.pctrLogotipo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pctrLogotipo.TabIndex = 1;
             this.pctrLogotipo.TabStop = false;
@@ -197,22 +209,24 @@
             // 
             this.pnlFormulario.BackColor = System.Drawing.SystemColors.Control;
             this.pnlFormulario.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlFormulario.Location = new System.Drawing.Point(264, 0);
-            this.pnlFormulario.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.pnlFormulario.Location = new System.Drawing.Point(352, 0);
+            this.pnlFormulario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlFormulario.MinimumSize = new System.Drawing.Size(1707, 886);
             this.pnlFormulario.Name = "pnlFormulario";
-            this.pnlFormulario.Size = new System.Drawing.Size(1280, 720);
+            this.pnlFormulario.Size = new System.Drawing.Size(1707, 886);
             this.pnlFormulario.TabIndex = 2;
             // 
             // frmClientePantallaPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(1685, 838);
             this.Controls.Add(this.pnlFormulario);
             this.Controls.Add(this.pnlMenuLateral);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(1023, 599);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(1358, 726);
             this.Name = "frmClientePantallaPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmClientePantallaPrincipal";
